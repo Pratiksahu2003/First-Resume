5753a7c72c7bb5002af8602afe56ae76f79354e1
Pratik Sahu <121413854+Pratiksahu2003@users.noreply.github.com>
2024-06-17 15:39:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        host: process.env.VITE_IP || 'localhost',
+        port: 5173,
+        strictPort: true
+    }
+
 });
